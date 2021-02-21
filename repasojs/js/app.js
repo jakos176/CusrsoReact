accb67ffb7239bbb70980ef4a757de219a383c7c
Edu <edumartsil95@tutanota.com>
2021-02-21 22:16:08 +0100

    Object Constructor

--- repasojs/js/app.js
+++ repasojs/js/app.js
@@ -5,14 +5,18 @@
 const persona = {
   nombre: "Juan",
   profesion: "Desarrollador web",
-  edad: 50
+  edad: 50,
 };
 
-let persona = "Juan";
-let profesion = "Desarrollador Web";
-
 console.log(persona);
-console.log(profesion);
-console.log(persona.nombre);
-console.log(persona.edad);
-console.log(persona[profesion]);
+
+//Object constructor
+
+function Tarea(nombre, urgencia) {
+  this.nombre = nombre;
+  this.urgencia = urgencia;
+}
+
+const tarea1 = new Tarea('Aprender Javascript y React', 'Urgente');
+console.log(tarea1);
+console.log(tarea1.nombre)
