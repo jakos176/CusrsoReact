3451b0a2395379d8579af2fe32c37e9072bc7cd9
Edu <edumartsil95@tutanota.com>
2021-02-26 22:54:43 +0100

    herencia en clases de javascript

--- repasojs/js/app.js
+++ repasojs/js/app.js
@@ -11,6 +11,17 @@ class Tarea {
     }
 }
 
+class comprasPendientes extends Tarea {
+    constructor(nombre, prioridad, cantidad) {
+        super(nombre, prioridad);
+        this.cantidad = cantidad;
+    }
+    mostrar() {
+        super.mostrar();
+        console.log(` y la cantidad de ${this.cantidad}`);
+    }
+}
+
 
 //crear los objetos
 let tarea1 = new Tarea('Aprender Javascript', 'Alta');
@@ -18,3 +29,8 @@ let tarea2 = new Tarea('Aprender PHP', 'Baja');
 
 tarea1.mostrar();
 tarea2.mostrar();
+
+//compras
+
+let compra1 = new comprasPendientes('Jabon', 'Alta', 2);
+compra1.mostrar();
