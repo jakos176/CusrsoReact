59787aa126b9b1a410ac421cb40b93dc766e8645
Edu <edumartsil95@tutanota.com>
2021-02-27 17:05:08 +0100

    Importar clases heradadas ES6

--- repasojs/js/compras.js
+++ repasojs/js/compras.js
@@ -1,4 +1,6 @@
-class comprasPendientes extends Tarea {
+import Tarea from "./tarea.js";
+
+export default class comprasPendientes extends Tarea {
     constructor(nombre, prioridad, cantidad) {
         super(nombre, prioridad);
         this.cantidad = cantidad;
